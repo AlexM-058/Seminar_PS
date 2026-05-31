14b8c0dae6027dfa3af3acbabaa0d0dca870f6a8
AlexM-058 <alexandrumateitarita@gmail.com>
2026-05-31 18:17:31 +0300

    Add basic data extraction functionality and restructure project -added extrageMeniu in DataExtraction.java

---
+++ src/main/java/org/example/DataExtraction.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+public class DataExtraction {
+
+    List<String> extrageMeniu(String url) {
+        List<String> liniiMeniu = new ArrayList<>();
+
+        try{
+           HttpClient client = HttpClient.newHttpClient();
+           HttpRequest request = HttpRequest.newBuilder()
+                   .uri(URI.create(url))
+                   .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            String html = response.body();
+
+            String[] bucatiHtml = html.split("<a ");
+
+            for (String bucata : bucatiHtml) {
+
+                int inceput = bucata.indexOf(">");
+                int sfarsit = bucata.indexOf("</a>");
+
+                if (inceput != -1 && sfarsit != -1  && inceput < sfarsit) {
+                    String linie = bucata.substring(inceput + 1, sfarsit);
+
+                    if(!linie.isEmpty() && !linie.contains("<") && linie.length() < 35){
+
+                    liniiMeniu.add(linie);
+                    }
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("Eroare la extragerea: " + e.getMessage());
+        }
+
+        return liniiMeniu;
+    }
+}
